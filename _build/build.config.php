d92963ea1c68982154a1d2bd24a67ca31691e37e
Alan Pich <alan.pich@gmail.com>
2013-06-10 12:12:06 +0100

    refactor build environment & script

---
+++ _build/build.config.php
@@ -0,0 +1,12 @@
+<?php
+define('PKG_NAME', 'GridEditor');
+define('PKG_NAMESPACE', 'grideditor');
+define('PKG_VERSION','1.0.2');
+define('PKG_RELEASE','beta');
+
+define('PKG_ROOT',dirname(dirname(__FILE__)).'/');
+define('PKG_CORE',PKG_ROOT.'core/components/'.PKG_NAMESPACE.'/');
+define('PKG_ASSETS',PKG_ROOT.'assets/components/'.PKG_NAMESPACE.'/');
+define('PKG_BUILD',PKG_ROOT.'_build/');
+define('PKG_COMMIT',Tools::getGitCommitId(PKG_ROOT));
+require PKG_ROOT.'config.core.php';
--- _build/build.transport.php
+++ _build/build.transport.php
@@ -1,95 +1,40 @@
 <?php
 /**
- * @package grideditor
- * @author Alan Pich
+ * Copyright 2013 by Alan Pich <alan.pich@gmail.com>
+ *
+ * This file is part of tvImagePlus
+ *
+ * tvImagePlus is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * tvImagePlus is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * tvImagePlus; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package tvImagePlus
+ * @author Alan Pich <alan.pich@gmail.com>
+ * @copyright Alan Pich 2013
  */
-header('Content-type: text/plain');
-$tstart = explode(' ', microtime());
-$tstart = $tstart[1] + $tstart[0];
-set_time_limit(0);
- 
-require_once dirname(__FILE__).'/build.tools.php';
 
-/* define package names */
-define('PKG_NAME','GridEditor');
-define('PKG_NAME_LOWER','grideditor');
-define('PKG_VERSION','1.0.1');
-define('PKG_RELEASE','rc1');
-define('PKG_COMMIT',getGitCommitId(dirname(dirname(__FILE__))));
+require dirname(__FILE__) . '/tools/build.tools.php';
+require dirname(__FILE__) . '/build.config.php';
+Tools::StartTimer();
 
-echo "Building from commit #".PKG_COMMIT."\n";
-/* define build paths */
-$root = '/var/www/modx/grideditor/';
-$build = dirname(__FILE__).'/';
-$sources = array(
-    'root' => $root,
-    'build' => $build,
-    'data' => $build.'data/',
-    'resolvers' => $build.'resolvers/',
-    'lexicon' => $root . 'core/components/'.PKG_NAME_LOWER.'/lexicon/',
-    'docs' => $root.'core/components/'.PKG_NAME_LOWER.'/docs/',
-    'elements' => $root.'core/components/'.PKG_NAME_LOWER.'/elements/',
-    'source_assets' => $root.'assets/components/'.PKG_NAME_LOWER,
-    'source_core' => $root.'core/components/'.PKG_NAME_LOWER,
-);
-unset($root);
-require_once $sources['root'] . 'config.core.php';
-require_once MODX_CORE_PATH . 'model/modx/modx.class.php';
- 
-// Set up a MODx Instance ======================================================
-$modx= new modX();
-$modx->initialize('mgr');
-echo '<pre>'; /* used for nice formatting of log messages */
-$modx->setLogLevel(modX::LOG_LEVEL_INFO);
-$modx->setLogTarget('ECHO');
 
-// Start Building the transport package ========================================
-$modx->loadClass('transport.modPackageBuilder','',false, true);
+// Create modx & package instance -----------------------------------------------------------------
+$modx = Tools::loadModxInstance();
 $builder = new modPackageBuilder($modx);
-$builder->createPackage(PKG_NAME_LOWER,PKG_VERSION,PKG_RELEASE);
-$builder->registerNamespace(PKG_NAME_LOWER,false,true,'{core_path}components/'.PKG_NAME_LOWER.'/');
- 
-// Create a Category for neatness ==============================================
-$category= $modx->newObject('modCategory');
-$category->set('id',1);
-$category->set('category',PKG_NAME);
+$builder->createPackage(PKG_NAMESPACE, PKG_VERSION, PKG_RELEASE);
 
-// Add in demo config chunk ====================================================
-$modx->log(modX::LOG_LEVEL_INFO,'Packaging in demo config chunk..');
-$chunks = array($modx->newObject('modChunk',array(
-        'name' => 'grideditor.config.demo',
-        'description' => 'Demo configuration file for GridEditor',
-        'snippet' => getSnippetContent($sources['elements'].'chunks/grideditor.config.demo.php')
-    )));
-$category->addMany($chunks);
 
-// Create Vehicle & add to package =============================================
-$attr = array(
-    xPDOTransport::UNIQUE_KEY => 'category',
-    xPDOTransport::PRESERVE_KEYS => false,
-    xPDOTransport::UPDATE_OBJECT => true,
-    xPDOTransport::RELATED_OBJECTS => true,
-    xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
-        'Chunks' => array(
-            xPDOTransport::PRESERVE_KEYS => false,
-            xPDOTransport::UPDATE_OBJECT => true,
-            xPDOTransport::UNIQUE_KEY => 'name',
-        ),
-    ),
-);
-$vehicle = $builder->createVehicle($category,$attr);
-
-// Add in file resolvers =======================================================
-$modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to package...');
-$vehicle->resolve('file',array(
-    'source' => $sources['source_assets'],
-    'target' => "return MODX_ASSETS_PATH . 'components/';",
-));
-$vehicle->resolve('file',array(
-    'source' => $sources['source_core'],
-    'target' => "return MODX_CORE_PATH . 'components/';",
-));
-$builder->putVehicle($vehicle);
+// Register Namespace -----------------------------------------------------------------------------
+$builder->registerNamespace(PKG_NAMESPACE, false, true, '{core_path}components/' . PKG_NAMESPACE . '/');
 
 
 // Add Action and Menu Item ====================================================
@@ -113,20 +58,77 @@ $builder->putVehicle($vehicle);
 unset($vehicle,$menu);
 
 
+// Create a Category for neatness ==============================================
+$category= $modx->newObject('modCategory');
+$category->set('id',1);
+$category->set('category',PKG_NAME);
+$attr = array(
+        xPDOTransport::UNIQUE_KEY => 'category',
+        xPDOTransport::PRESERVE_KEYS => false,
+        xPDOTransport::UPDATE_OBJECT => true,
+        xPDOTransport::RELATED_OBJECTS => true,
+        xPDOTransport::RELATED_OBJECT_ATTRIBUTES => array (
+            'Chunks' => array(
+                xPDOTransport::PRESERVE_KEYS => false,
+                xPDOTransport::UPDATE_OBJECT => true,
+                xPDOTransport::UNIQUE_KEY => 'name',
+            ),
+        ),
+    );
+
+
+
+// Add in demo config chunk ====================================================
+$modx->log(modX::LOG_LEVEL_INFO,'Packaging in demo config chunk..');
+$chunks = array($modx->newObject('modChunk',array(
+        'name' => 'grideditor.config.demo',
+        'description' => 'Demo configuration file for GridEditor',
+        'snippet' => getSnippetContent($sources['elements'].'chunks/grideditor.config.demo.php')
+    )));
+$category->addMany($chunks);
+
+
+// Create Vehicle & add to package =============================================
+$vehicle = $builder->createVehicle($category,$attr);
+
+
+// Add in file resolvers =======================================================
+$modx->log(modX::LOG_LEVEL_INFO,'Adding file resolvers to package...');
+$vehicle->resolve('file',array(
+    'source' => PKG_ASSETS,
+    'target' => "return MODX_ASSETS_PATH . 'components/';",
+));
+$vehicle->resolve('file',array(
+    'source' => PKG_CORE,
+    'target' => "return MODX_CORE_PATH . 'components/';",
+));
+$builder->putVehicle($vehicle);
+
 
 // Add documentation ===========================================================
-$modx->log(modX::LOG_LEVEL_INFO,'Adding documentation...');
-$builder->setPackageAttributes(array(
-    'license' => file_get_contents($sources['docs'] . 'license.txt'),
-    'readme' => getReadmeFile($sources['docs'] . 'readme.tpl'),
-    'changelog' => file_get_contents($sources['docs'] . 'changelog.txt')
-));
+$modx->log(modX::LOG_LEVEL_INFO, 'Adding documentation...');
+$builder->setPackageAttributes(
+    array(
+        'license' => file_get_contents($sources['docs'] . 'license.txt'),
+        'readme' => Tools::parseReadmeTpl($sources['docs'] . 'readme.tpl'),
+        'changelog' => file_get_contents($sources['docs'] . 'changelog.txt'),
+    )
+);
 
-// Zip up the package ==========================================================
-$modx->log(modX::LOG_LEVEL_INFO,'Packing up transport package zip...');
+// Create transport package -----------------------------------------------------------------------
+$modx->log(modX::LOG_LEVEL_INFO, 'Packing component for transport...');
 $builder->pack();
- 
-$tend= explode(" ", microtime());
-$tend= $tend[1] + $tend[0];
-$totalTime= sprintf("%2.4f s",($tend - $tstart));
-$modx->log(modX::LOG_LEVEL_INFO,"\n<br />Package Built.<br />\nExecution time: {$totalTime}\n");
+
+
+// Copy transport package back to PKG_ROOT --------------------------------------------------------
+$zipFile = PKG_NAMESPACE.'-'.PKG_VERSION.'-'.PKG_RELEASE.'.transport.zip';
+$zipPath = MODX_CORE_PATH.'packages/'. $zipFile;
+copy($zipPath,PKG_ROOT.$zipFile);
+
+
+// Build process finished -------------------------------------------------------------------------
+$totalTime= sprintf("%2.4f s", Tools::stopTimer());
+$modx->log(modX::LOG_LEVEL_INFO,"Package ".PKG_NAME.' '.PKG_VERSION.'-'.PKG_RELEASE." built in {$totalTime}");
+
+
+exit;
